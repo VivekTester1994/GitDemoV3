1752e06df3edd5e7cb885138ca8cda4d3c71b297
vivek <vivekjacob1994@hotmail.com>
2025-05-14 14:12:26 +0800

    Commit to YBranch

---
+++ src/Package1/Lesson2_J.java
@@ -0,0 +1,15 @@
+package Package1;
+
+public class Lesson2_J {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="Mark";
+		if(name=="Mark"||name=="Jacob"||name=="Sam")
+			System.out.println("thgis is my name");
+		else {
+			System.out.println("this is not my name");
+		}
+	}
+
+}
