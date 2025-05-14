b18184ae7f691bc32d0bbfdcff2081e999ea0d57
vivek <vivekjacob1994@hotmail.com>
2025-05-14 16:21:20 +0800

    The new Lesson

--- src/Package1/lesson3_J.java
+++ src/Package1/lesson3_J.java
@@ -1,10 +1,21 @@
 package Package1;
 
+import java.util.Scanner;
+
 public class lesson3_J {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
+		 Scanner scanner = new Scanner(System.in);
 
+	        System.out.print("Enter your name: ");
+	        String name = scanner.nextLine();
+	        System.out.println("Your name is: " + name);
+
+	        System.out.print("Enter your age: ");
+	        int age = scanner.nextInt();
+	        System.out.println("Your age is: " + age);
 	}
 
 }
