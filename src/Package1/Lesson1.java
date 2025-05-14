0b0b5ac6ec5edb2c8cd6c2d65bc40b6f8bc73ee3
vivek <vivekjacob1994@hotmail.com>
2025-05-14 14:31:34 +0800

    NewChanges

--- src/Package1/Lesson1.java
+++ src/Package1/Lesson1.java
@@ -5,7 +5,7 @@ public class Lesson1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String name="Mark";
-		if(name=="Mark"||name=="Jacob"||name=="Sam")
+		if(name=="Mark")
 			System.out.println("thgis is my name");
 		else {
 			System.out.println("this is not my name");
--- src/Package1/Lesson2_J.java
+++ src/Package1/Lesson2_J.java
@@ -5,7 +5,7 @@ public class Lesson2_J {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String name="Mark";
-		if(name=="Mark"||name=="Jacob"||name=="Sam")
+		if(name=="Mark"||name=="Sneha")
 			System.out.println("thgis is my name");
 		else {
 			System.out.println("this is not my name");
